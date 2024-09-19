1d28bb31589e5ce90de651bbf397fb23099f7cbe
Moritz Opitz <moritzopitz@MacBook-Pro-von-Moritz.fritz.box>
2024-09-19 10:37:51 +0200

    adjust database URL

--- api/database.py
+++ api/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine, inspect, text
 import traceback
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
 import os
 
 
