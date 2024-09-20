99d462fad3d3d7a0e86fe12617c355f42160048f
Moritz Opitz <moritzopitz@MBP-von-Moritz.fritz.box>
2024-09-20 18:36:04 +0200

    new tests 7

--- api/database.py
+++ api/database.py
@@ -4,7 +4,7 @@ import os
 
 
 # database session
-SQLALCHEMY_DATABASE_URL = os.getenv('DB_CONNECTION_URL')
+SQLALCHEMY_DATABASE_URL = os.getenv('postgresql://pg-user:%3BBAN%40535pVGfCZbl@127.0.0.1:5432/postgres')
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
